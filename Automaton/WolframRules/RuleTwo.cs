b75aa733d3f0427b34c5f50bf45fad1507bb5fd7
GorginZ <georgia.z.leng@gmail.com>
2020-12-26 14:28:29 +1100

    rules dir

---
+++ Automaton/WolframRules/RuleTwo.cs
@@ -0,0 +1,10 @@
+namespace Automaton.WolframRules
+{
+  public class RuleTwo : IRules
+  {
+    public int CellValueForNextGen(int left, int self, int right)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
